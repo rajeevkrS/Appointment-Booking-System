31930307b840ae4dcddf151356fa5f3c39db8df0
Rajeev <ronaldocrcr5@gmail.com>
2024-10-14 14:58:37 +0530

    successfully created footer ui component

--- frontend/src/components/Faq.jsx
+++ frontend/src/components/Faq.jsx
@@ -3,7 +3,7 @@ import FaqItems from "./FaqItems";
 
 const Faq = () => {
   return (
-    <div className="max-w-full w-[1440px] px-10 mx-auto">
+    <div className="max-w-full w-[1440px] p-10 mx-auto bg-blue-50 rounded-xl">
       <div className="flex justify-between w-full gap-[50px] lg:gap-0 ">
         <div className="w-[40%] hidden md:block">
           <img src={assets.faq_img} alt="" />
