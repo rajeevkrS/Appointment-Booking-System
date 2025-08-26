8a7c72dcf265529b5d03846fcf16aec3a4779fb4
Rajeev Kumar Sudhansu <rajeevkumarr1221@gmail.com>
2025-08-26 13:46:51 +0530

    created the auth doctor middleware for doctors authentication

---
+++ backend/middlewares/authDoctor.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebdrtoken";
+
+// Doctor authentication middleware
+const authDoctor = async (req, res, next) => {
+  try {
+    const { drtoken } = req.headers;
+
+    // If drtoken is not present
+    if (!drtoken) {
+      return res.json({
+        succes: false,
+        message: "Not authorized, Login again!",
+      });
+    }
+
+    // Verifying the drtoken
+    const drtoken_decode = jwt.verify(drtoken, process.env.JWT_SECRET);
+
+    // got the doctor id from the drtoken and it will be added in the req.body
+    req.doctor = { id: drtoken_decode.id };
+
+    // Call the next middleware or route handler
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({ succes: false, message: error.message });
+  }
+};
+
+export default authDoctor;
